bc492b0b6e0635fe0eb0dc0992d0afde13ee7aaf
Özgür Salih Dülger <120192955+ozgursalihdulger@users.noreply.github.com>
2023-01-18 02:04:22 +0300

    Add files via upload

---
+++ FootballPlayerBook/FootballPlayerBook/DetailsVC.swift
@@ -0,0 +1,136 @@
+//
+//  DetailsVC.swift
+//  FootballPlayerBook
+//
+//  Created by Özgür Salih Dülger on 17.01.2023.
+//
+
+import UIKit
+import CoreData
+
+class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameText: UITextField!
+
+    @IBOutlet weak var teamText: UITextField!
+    
+    @IBOutlet weak var ageText: UITextField!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    var chosenPainting = ""
+    var chosenPaintingId : UUID?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if chosenPainting != "" {
+            
+            saveButton.isHidden = true
+            
+            
+            
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            
+            let idString = chosenPaintingId?.uuidString
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString!)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do{
+                let results = try context.fetch(fetchRequest)
+                
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject]{
+                        if let name = result.value(forKey: "name") as? String {
+                            nameText.text = name
+                        }
+                        if let team = result.value(forKey: "team") as? String {
+                            teamText.text = team
+                        }
+                        if let age = result.value(forKey: "age") as? Int {
+                            ageText.text = String(age)
+                        }
+                        if let imageData = result.value(forKey: "image") as? Data {
+                            let image = UIImage(data: imageData)
+                            imageView.image = image
+                        }
+                        
+                    }
+                    
+                }
+                
+            }catch {
+                print("Error")
+            }
+        }
+        
+        
+        
+        //Recognizers
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(gestureRecognizer)
+        
+        imageView.isUserInteractionEnabled = true
+        let imageTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(imageTapRecognizer )
+
+    }
+    
+    @objc func selectImage() {
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true)
+    }
+    
+    
+    @objc func hideKeyboard() {
+        view.endEditing(true)
+    }
+    
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newPainting = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
+        
+        //Attriutes
+        newPainting.setValue(nameText.text!, forKey: "name")
+        newPainting.setValue(teamText.text!, forKey: "team")
+        
+        if let age = Int(ageText.text!){
+            newPainting.setValue(age, forKey: "age")
+        }
+        
+        newPainting.setValue(UUID(), forKey: "id")
+        
+        let data = imageView.image!.jpegData(compressionQuality: 0.5)
+        newPainting.setValue(data, forKey: "image")
+        
+        do {
+            try context.save()
+            print("Successfully Saved")
+        } catch {
+            print("Error")
+        }
+        
+        
+        NotificationCenter.default.post(name: NSNotification.Name("newData"), object: nil)
+        self.navigationController?.popViewController(animated: true)
+        
+    }
+    
+    
+}
